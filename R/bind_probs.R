711af6f2e6d29bb18b4c7c8c686803ed47ff9791
Bernardo Reckziegel <bernardo_cse@hotmail.com>
2021-11-19 18:09:19 -0300

    `bind_probs()` returns the user original call in the 3º column.

--- R/bind_probs.R
+++ R/bind_probs.R
@@ -30,37 +30,45 @@
 #' bind_probs(x, y) %>%
 #'   ggplot(aes(x = rowid, y = probs, color = fn)) +
 #'   geom_line() +
-#'   scale_color_viridis_d()
+#'   scale_color_viridis_d() +
+#'   theme(legend.position="bottom")
 bind_probs <- function(...) {
 
- dots <- rlang::list2(...)
- dots <- purrr::keep(dots, inherits, "ffp")
+    dots <- rlang::list2(...)
+    dots <- purrr::keep(dots, inherits, "ffp")
 
- # TODO add a warning when a list is discarded.
+    # TODO add a warning when a list is discarded.
 
- if (is_empty(dots)) {
-   stop("objects to bind must be of the `ffp` class.", call. = FALSE)
- }
+    if (is_empty(dots)) {
+        stop("objects to bind must be of the `ffp` class.", call. = FALSE)
+    }
 
- unique_rows <- unique(purrr::map_dbl(dots, vctrs::vec_size))
- if (length(unique_rows) > 1) {
-   stop("Arguments to bind must have the same size (rows).", call. = FALSE)
- }
+    unique_rows <- unique(purrr::map_dbl(dots, vctrs::vec_size))
+    if (length(unique_rows) > 1) {
+        stop("Arguments to bind must have the same size (rows).", call. = FALSE)
+    }
 
- attr_fn   <- purrr::map(purrr::map(dots, attributes), 1)
- attr_list <- purrr::map(purrr::map(dots, attributes), 2) %>% purrr::map(as.list)
- attr_nms  <- purrr::map(attr_list, names) %>% purrr::map(3)
- attr_vl   <- purrr::map(attr_list, 3)
- fn <- paste0(as.character(attr_fn), ": ", as.character(attr_nms), " = ", as.vector(attr_vl))
- fn <- rep(fn, each = unique_rows)
+    # attr_fn   <- purrr::map(purrr::map(dots, attributes), 1)
+    # attr_list <- purrr::map(purrr::map(dots, attributes), 2) %>% purrr::map(as.list)
+    # attr_nms  <- purrr::map(attr_list, names) %>% purrr::map(3)
+    # attr_vl   <- purrr::map(attr_list, 3)
+    # fn <- paste0(as.character(attr_fn), ": ", as.character(attr_nms), " = ", as.vector(attr_vl))
+    fn <- as.character(purrr::map(purrr::map(dots, attributes), "user_call"))
+    fn <- stringr::str_remove(fn, ".numeric") %>%
+        stringr::str_remove(pattern = ".matrix") %>%
+        stringr::str_remove(pattern = ".ts") %>%
+        stringr::str_remove(pattern = ".xts") %>%
+        stringr::str_remove(pattern = ".tbl_df") %>%
+        stringr::str_remove(pattern = ".data.frame")
+    fn <- rep(fn, each = unique_rows)
 
- seq_to_add  <- rep(1:length(dots), each = unique_rows)
+    seq_to_add  <- rep(1:length(dots), each = unique_rows)
 
- purrr::map(dots, tibble::as_tibble) %>%
-    purrr::map(tibble::rowid_to_column) %>%
-    dplyr::bind_rows() %>%
-    dplyr::rename(probs = "value") %>%
-    dplyr::mutate(fn = as.factor(fn))
+    purrr::map(dots, tibble::as_tibble) %>%
+        purrr::map(tibble::rowid_to_column) %>%
+        dplyr::bind_rows() %>%
+        dplyr::rename(probs = "value") %>%
+        dplyr::mutate(fn = as.factor(fn))
 
 }
 
